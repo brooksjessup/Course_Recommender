8f0774f983f54436d682d20e5897bbd6515f1307
Brooks Jessup <brooks.jessup@gmail.com>
2020-06-07 15:41:29 -0700

    files for deployment

--- API/cr_app.py
+++ API/cr_app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 from flask import Flask, request, Response, render_template, jsonify
 import gensim
+import pandas as pd
 
 # initialize the flask app
 app = Flask('cr_app')
@@ -27,8 +28,12 @@ def submit():
     doc = gensim.utils.simple_preprocess(js) # Preprocess the job description
     model = pickle.load(open('./model.p', 'rb')) # Load the model
     vector = model.infer_vector(doc) # Vectorize the job description
-    sims = model.docvecs.most_similar([vector])
-    return render_template('results.html', recommendation=sims)
+    top = 10 # Set how many courses to recommend
+    sims = model.docvecs.most_similar([vector], topn=top) # Find the most similar vectors
+    course_ids = [sim[0] for sim in sims] # Extract the course index numbers
+    df = pd.read_csv('../Data/Course_Data/Coursera_Catalog.csv') # Read in course data
+    course_names = [df.iloc[id]['name'] for id in course_ids] # Get the course names
+    return render_template('results.html', recommendations=course_names, len=top)
 
 # Call app.run(debug=True) when python script is called
 if __name__ == '__main__': # if you run 'python app_starter.py' from terminal
