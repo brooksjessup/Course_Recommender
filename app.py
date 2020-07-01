9c9079f234f12dcff6aec873377e1859a3cc9b19
Brooks Jessup <brooks.jessup@gmail.com>
2020-06-30 18:09:34 -0700

    update

--- app.py
+++ app.py
@@ -17,16 +17,17 @@ def home():
 # route 2: accept the form submission and do something fancy with it
 @app.route('/submit')
 def submit():
-    js = request.args["JobDesc"] # Load in the form data
-    doc = gensim.utils.simple_preprocess(js) # Preprocess the job description
+    jd = request.args["JobDesc"] # Load in the form data
+    doc = gensim.utils.simple_preprocess(jd) # Preprocess the job description
     model = pickle.load(open('./model.p', 'rb')) # Load the model
     vector = model.infer_vector(doc) # Vectorize the job description
-    top = 10 # Set how many courses to recommend
+    top = 5 # Set how many courses to recommend
     sims = model.docvecs.most_similar([vector], topn=top) # Find the most similar vectors
     course_ids = [sim[0] for sim in sims] # Extract the course index numbers
     df = pd.read_csv('./Data/Course_Data/Coursera_Catalog.csv') # Read in course data
     course_names = [df.iloc[id]['name'] for id in course_ids] # Get the course names
-    return render_template('results.html', recommendations=course_names, len=top)
+    course_descriptions = [df.iloc[id]['description'] for id in course_ids] # Get the course descriptions
+    return render_template('results.html', len=top, names=course_names, descriptions=course_descriptions)
 
 # Call app.run(debug=True) when python script is called
 if __name__ == '__main__':
